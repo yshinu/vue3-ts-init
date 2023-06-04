8e365da6e0cab3826a5cf28f7322c8089d59f5bf
Yshinu <yshinu2833@gmail.com>
2023-06-04 21:04:42 +0800

    feat: init

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-
+import 'amfe-flexible';
 import App from './App.vue';
 import router from './router';
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,58 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path'; //这个path用到了上面安装的@types/node
+import viteCompression from 'vite-plugin-compression';
+import postCssPxToRem from 'postcss-pxtorem';
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'), // @代替src
-      '#': path.resolve('./types'), // #代替types
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@/assets/styles/index.scss";',
+export default ({ mode }) => {
+  return defineConfig({
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        },
       },
     },
-  },
-});
+    plugins: [
+      vue(),
+      {
+        ...viteCompression(),
+        apply: 'build',
+      },
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'), // @代替src
+        '#': path.resolve('./types'), // #代替types
+      },
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: loadEnv(mode, process.cwd()).VITE_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    css: {
+      postcss: {
+        plugins: [
+          postCssPxToRem({
+            rootValue: 16,
+            propList: ['*'],
+            exclude: './node_modules/',
+          }),
+        ],
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/styles/index.scss";',
+        },
+      },
+    },
+  });
+};
